299cfccf5576aef45cb97e1a118f28775da947b1
邵心艺 <shaoxinyi@sxy.local>
2026-09-18 14:37:39 +0800

    Keep Vivian umbrella fully visible on mobile

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "qingcheng-v9";
+const CACHE = "qingcheng-v10";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icons/icon.svg", "./icons/apple-touch-icon.png", "./assets/vivian-art.png", "./assets/vivian-expressions.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
